074011cc20a7e6d23c7e749ed21a37dc8b61f6f0
VKalenski <vilislav.kalenski@elando.com>
2022-08-02 16:50:50 +0300

    Add more props

--- Models/Character.cs
+++ Models/Character.cs
@@ -20,5 +20,9 @@ namespace Models
         public int Intelligence { get; set; } = 10;
 
         public RpgClass Class { get; set; } = RpgClass.Knight;
+
+        public User? User {get; set;}
+
+        public Weapon Weapon { get; set; }
     }
 }
