a5f97de74c86d3e66e6c27df186f021fa1078655
VKalenski <vilislav.kalenski@elando.com>
2022-08-02 16:56:32 +0300

    Add new migrations

--- Migrations/20220802075020_UserCharacterRelationship.Designer.cs
+++ Migrations/20220802075020_UserCharacterRelationship.Designer.cs
@@ -4,6 +4,7 @@ using Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace _NET_6_Web_Api.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220802075020_UserCharacterRelationship")]
+    partial class UserCharacterRelationship
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -84,32 +86,6 @@ namespace _NET_6_Web_Api.Migrations
                     b.ToTable("Users");
                 });
 
-            modelBuilder.Entity("Models.Weapon", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
-
-                    b.Property<int>("CharacterId")
-                        .HasColumnType("int");
-
-                    b.Property<int>("Damage")
-                        .HasColumnType("int");
-
-                    b.Property<string>("Name")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.HasKey("Id");
-
-                    b.HasIndex("CharacterId")
-                        .IsUnique();
-
-                    b.ToTable("Weapons");
-                });
-
             modelBuilder.Entity("Models.Character", b =>
                 {
                     b.HasOne("Models.User", "User")
@@ -117,23 +93,6 @@ namespace _NET_6_Web_Api.Migrations
                         .HasForeignKey("UserId");
 
                     b.Navigation("User");
-                });
-
-            modelBuilder.Entity("Models.Weapon", b =>
-                {
-                    b.HasOne("Models.Character", "Character")
-                        .WithOne("Weapon")
-                        .HasForeignKey("Models.Weapon", "CharacterId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
-                    b.Navigation("Character");
-                });
-
-            modelBuilder.Entity("Models.Character", b =>
-                {
-                    b.Navigation("Weapon")
-                        .IsRequired();
                 });
 
             modelBuilder.Entity("Models.User", b =>
