e2ce32c7dabd69210a23184102071c7d5b8a713f
VKalenski <vilislav.kalenski@elando.com>
2022-08-03 12:57:42 +0300

    Add new methods

--- Services/CharacterService/ICharacterService.cs
+++ Services/CharacterService/ICharacterService.cs
@@ -18,5 +18,7 @@ namespace Service.CharacterService
         Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCracterDto updateCracterDto);
 
         Task<ServiceResponse<List<GetCharacterDto>>> DeleteCharacter(int id);
+
+        Task<ServiceResponse<GetCharacterDto>> AddCharacterSkill(AddCharacterSkillDto newCharacterSkill);
     }
 }
