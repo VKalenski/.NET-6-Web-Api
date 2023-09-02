331c02faba9f35fbf7ff11f22633819aa6a0b8f5
Vilislav Kalenski <71873406+VKalenski@users.noreply.github.com>
2023-09-02 12:24:34 +0300

    Add Models, Dtos/Character and Skill

--- Dtos/Character/GetCharacterDto.cs
+++ Dtos/Character/GetCharacterDto.cs
@@ -1,11 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Dtos.Skill;
-using Dtos.Weapon;
-using Models;
-
 namespace Dtos.Character
 {
     public class GetCharacterDto
