bd0a62117e891d5571ff6b0857e9fd0afce4a835
Vilislav Kalenski <71873406+VKalenski@users.noreply.github.com>
2023-09-06 22:40:05 +0300

    Refactoring the code

--- Dtos/Character/AddCharacterDto.cs
+++ Dtos/Character/AddCharacterDto.cs
@@ -4,7 +4,7 @@ namespace Dtos.Character
     {
         public string Name { get; set; } = "Frodo";
 
-        public int HitPoints { get; set; } = 100; 
+        public int HitPoints { get; set; } = 100;
 
         public int Strength { get; set; } = 10;
 
--- Dtos/Fight/HighscoreDto.cs
+++ Dtos/Fight/HighscoreDto.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Dtos.Fight
 {
     public class HighscoreDto
     {
         public int Id { get; set; }
 
-        public string Name { get; set; } = string.Empty; 
+        public string Name { get; set; } = string.Empty;
 
         public int Fights { get; set; }
 
